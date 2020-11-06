b1eae09bdd7419e84be04ed5fff4c87f076f7fc2
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-11-06 17:38:20 +0700

    feat: home layout

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) => {
-  res.render('index', { title: 'ccccc' });
+  res.render('home/index');
 });
 
 export default indexRouter;
