4cccb2fa0cb24c9022e99ebce5702483c484ef7f
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-11-07 00:53:32 +0700

    feat: finish home page

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,5 +1,5 @@
-import { getBuyingMost, getRecentProduct } from "../models/homeModels";
+import { getBuyingMost, getRecentProduct, getCategoryLeftBar, getReviewOfCustomer } from "../models/homeModels";
 
 export const getHomePage = (req, res) => {
-    res.render('home/index', { buyingMost: getBuyingMost(), recentProduct: getRecentProduct() });
+    res.render('home/index', { buyingMost: getBuyingMost(), recentProduct: getRecentProduct(), categoryLeftBar: getCategoryLeftBar(), review: getReviewOfCustomer() });
 }
