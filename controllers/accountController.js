7887a14005e666bdf375999081c34a882d6447ea
widattt <45599756+widattt@users.noreply.github.com>
2020-11-10 19:09:20 +0700

    Login

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,11 +1,33 @@
+import { compile } from "morgan";
+import { getAccount } from "../models/accountModels";
+import { getBuyingMost, getRecentProduct, getCategoryLeftBar, getReviewOfCustomer } from "../models/homeModels";
+
+// import { getAccount } from "../models/accountModels";
 export const getAccountPage = (req, res) => {
-    res.render("account/account", { title: "Tài khoản" });
-}
+  res.render("account/account", { title: "Tài khoản" });
+};
 
 export const getLoginPage = (req, res) => {
-    res.render("account/login", { title: "Đăng kí" });
-}
+  res.render("account/login", { layout: "layout/loginlayout" });
+};
+
+export const postLoginPage = (req, res) => {
+    var username=req.body.username;
+    var pass=req.body.pass;
+    var getAcc=getAccount();
+    var account = getAcc.find( ele => ele.username === username);
+    console.log(account);
+    if(!account){
+        res.render("account/login",{ Noti: "Người dùng không tồn tại",layout: "layout/loginlayout" });
+        return;
+    }
+    if(account.password!==pass){
+        res.render("account/login",{ Noti: "Bạn đã nhập sai mật khẩu", layout: "layout/loginlayout" })
+        return;
+    }
+    res.render("home/index",{ title: "Trang chủ", buyingMost: getBuyingMost(), recentProduct: getRecentProduct(), categoryLeftBar: getCategoryLeftBar(), review: getReviewOfCustomer() });
+  };
 
 export const getRegisterPage = (req, res) => {
-    res.render("account/login", { title: "Đăng nhập" });
-}
+  res.render("account/register", { title: "Đăng nhập" });
+};
--- routes/index/accountRouter.js
+++ routes/index/accountRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAccountPage, getLoginPage, getRegisterPage } from "../../controllers/accountController";
+import { getAccountPage, getLoginPage, getRegisterPage,postLoginPage } from "../../controllers/accountController";
 const accountRouter = express.Router();
 
 accountRouter.get("/", getAccountPage);
 accountRouter.get("/login", getLoginPage);
+accountRouter.post("/login", postLoginPage);
 accountRouter.get("/register", getRegisterPage);
+
 
 export default accountRouter;
