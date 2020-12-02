22d8e3722e85024a4727d594e1f29f040d299f26
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-12-02 21:07:53 +0700

    fix: heroku deloy with no env file

--- database/config.js
+++ database/config.js
@@ -3,7 +3,8 @@ dotenv.config();
 module.exports = {
   "development": {
     use_env_variable: true,
-    "url": "mysql://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASENAME,
+    // "url": "mysql://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASENAME,
+    "url": "mysql://ishopz:Gg4DQV!!K03j@den1.mysql2.gear.host:3306/ishopz",
     "dialect": "mysql",
     "timezone": "+07:00" //for writing to database
   },
