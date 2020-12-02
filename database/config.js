8f4f29763303871de54cc748f99b6cc2d11faf9f
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-12-02 21:29:49 +0700

    fix: db

--- database/config.js
+++ database/config.js
@@ -3,8 +3,7 @@ dotenv.config();
 module.exports = {
   "development": {
     use_env_variable: true,
-    // "url": "mysql://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASENAME,
-    "url": "mysql://ishopz:Gg4DQV!!K03j@den1.mysql2.gear.host:3306/ishopz",
+    "url": "mysql://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_HOST + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASENAME,
     "dialect": "mysql",
     "timezone": "+07:00" //for writing to database
   },
