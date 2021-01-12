1ee97480b1d5e43db065913dcba2c5fcae7beef1
widattt <45599756+widattt@users.noreply.github.com>
2021-01-12 21:47:16 +0700

    fix: router for product

--- routes/index/productRouter.js
+++ routes/index/productRouter.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { getProductDetailPage, getProductListPage, getProductListPagination } from "../../controllers/productController";
+import {
+    getProductDetailPage,
+    getProductListPageByCategoryPage,
+    getProductListPage,
+    getProductListPageBySearchText,
+    addACommentAction
+} from "../../controllers/productController";
 const productRouter = express.Router();
 
 productRouter.get("/", getProductListPage);
-productRouter.get("/page/:id", getProductListPagination);
+productRouter.get("/category/:id", getProductListPageByCategoryPage);
+productRouter.get("/search", getProductListPageBySearchText);
 productRouter.get("/detail/:id", getProductDetailPage);
+productRouter.post("/comment/:id", addACommentAction);
 
 export default productRouter;
