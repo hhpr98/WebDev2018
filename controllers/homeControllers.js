738ca50a1dde8f515134209337f766ed0fd22d4c
widattt <45599756+widattt@users.noreply.github.com>
2021-01-12 21:44:18 +0700

    feat: home page controller

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,13 +1,23 @@
 import { getBuyingMost, getRecentProduct, getCategoryLeftBar, getReviewOfCustomer } from "../models/homeModels";
+import catchAsync from "../libs/catchAsync";
 
-export const getHomePage = (req, res) => {
-    res.render("home/index", { title: "Trang chủ", buyingMost: getBuyingMost(), recentProduct: getRecentProduct(), categoryLeftBar: getCategoryLeftBar(), review: getReviewOfCustomer() });
-}
+export const getHomePage = catchAsync(
+    async (req, res) => {
+        const categoryLeftBar = getCategoryLeftBar();
+        const recentProduct = await getRecentProduct();
+        const buyingMost = await getBuyingMost();
+        res.render("home/index", { title: "Trang chủ", buyingMost, recentProduct, categoryLeftBar, review: getReviewOfCustomer() });
+    }
+);
 
-export const getWishlistPage = (req, res) => {
-    res.render("other/wishlist", { title: "Wishlist Page" });
-}
+export const getWishlistPage = catchAsync(
+    async (req, res) => {
+        res.render("other/wishlist", { title: "Yêu thích" });
+    }
+);
 
-export const getContactPage = (req, res) => {
-    res.render("other/contact", { title: "Liên hệ" });
-}
+export const getContactPage = catchAsync(
+    async (req, res) => {
+        res.render("other/contact", { title: "Liên hệ" });
+    }
+);
